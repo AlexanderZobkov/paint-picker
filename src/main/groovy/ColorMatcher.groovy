bb460f8761cfc8e771a532b4d6144ec67ff8d0c3
Alexander Zobkov <alexander.zobkov@gmail.com>
2020-03-27 14:14:52 +0300

    Add javadoc here and there

--- src/main/groovy/ColorMatcher.groovy
+++ src/main/groovy/ColorMatcher.groovy
@@ -1,5 +1,15 @@
+/**
+ * Allows to find closest matching colors.
+ */
 interface ColorMatcher {
 
+    /**
+     * Finds closest matching colors for the specified color.
+     *
+     * @param color a color for which need to find closest matches.
+     * @param palette a list of colors where to find closest color.
+     * @return List of closest matched colors.
+     */
     List<Tuple2<NamedColor, Float>> findClosestMatch(NamedColor color, List<NamedColor> palette)
 
 }
